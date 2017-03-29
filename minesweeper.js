81756210e8e60af0b5e98d09eaa463d3f3753c6d
natalie-perret <nperret@gmail.com>
2017-03-30 12:06:17 +1300

    Add newBoard and resetButton

--- minesweeper.js
+++ minesweeper.js
@@ -1,75 +1,15 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
+var size = 4;
+var totalMines = 3;
 var board = {
-  "cells": [
-    {
-      row:0,
-      col:0,
-      isMarked:false,
-      isMine:true,
-      hidden:true
-    },
-    {
-      row:0,
-      col:1,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    },
-    {
-      row:0,
-      col:2,
-      isMarked: false,
-      isMine:true,
-      hidden:true
-    },
-    {
-      row:1,
-      col:0,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    },
-    {
-      row:1,
-      col:1,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    },
-    {
-      row:1,
-      col:2,
-      isMarked:false,
-      isMine:true,
-      hidden:true
-    },
-    {
-      row:2,
-      col:0,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    },
-    {
-      row:2,
-      col:1,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    },
-    {
-      row:2,
-      col:2,
-      isMarked:false,
-      isMine:false,
-      hidden:true
-    }
-  ]
+  cells: []
 };
 
+
 function startGame () {
+  newBoard(size);
   for (var i=0; i<board.cells.length; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
@@ -78,6 +18,36 @@ function startGame () {
 
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+}
+
+function newBoard(size){
+  for (var i = 0; i <= size; i++){
+    for (var j = 0; j<= size; j++){
+      var newCell = {
+        row: i,
+        col: j,
+        isMine: false,
+        isMarked: false,
+        hidden: true,
+        surroundingMines: 0
+      }
+    board.cells.push(newCell)
+    }
+  }
+  var mineCount = 0
+  while (mineCount < totalMines) {
+  var randomNum = Math.floor(Math.random() *  (size*size))
+    if (!board.cells[randomNum].isMine) {
+      board.cells[randomNum].isMine = true
+      mineCount++
+    }
+  }
+}
+
+//attempt at resetting game
+
+function resetGame() {
+    location.reload();
 }
 
 // Define this function to look for a win condition:
@@ -93,7 +63,7 @@ function checkForWin () {
           return
       }
     }
-    lib.displayMessage('You win!');
+    lib.displayMessage('You saved us!');
     return;
   }
 
